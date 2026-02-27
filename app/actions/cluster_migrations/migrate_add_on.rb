427a53cd342b2589ca40e374cc467023d957e568
Chris Zhu <chris_zhu12@hotmail.com>
2026-02-27 01:15:37 -0800

    update cluster migrations

--- app/actions/cluster_migrations/migrate_add_on.rb
+++ app/actions/cluster_migrations/migrate_add_on.rb
@@ -2,6 +2,7 @@ class ClusterMigrations::MigrateAddOn
   extend LightService::Action
 
   expects :source_add_on, :target_cluster
+  expects :custom_name, :custom_namespace, :managed_namespace
   promises :migrated_add_on
 
   executed do |context|
@@ -15,12 +16,13 @@ class ClusterMigrations::MigrateAddOn
       add_on.metadata = (source.metadata || {}).merge("install_stage" => 0)
 
       # Generate unique name within the account
-      base_name = source.name
+      base_name = context.custom_name || source.name
       add_on.name = base_name
       while AddOn.joins(:cluster).where(clusters: { account_id: target_cluster.account_id }, name: add_on.name).exists?
         add_on.name = "#{base_name}-#{SecureRandom.hex(4)}"
       end
-      add_on.namespace = add_on.name
+      add_on.namespace = context.custom_namespace || add_on.name
+      add_on.managed_namespace = context.managed_namespace if context.custom_namespace.present?
       add_on.save!
 
       context.migrated_add_on = add_on
--- app/actions/cluster_migrations/migrate_project.rb
+++ app/actions/cluster_migrations/migrate_project.rb
@@ -1,8 +1,8 @@
 class ClusterMigrations::MigrateProject
   extend LightService::Organizer
 
-  def self.call(source_project:, target_cluster:)
-    with(source_project:, target_cluster:).reduce(
+  def self.call(source_project:, target_cluster:, custom_name: nil, custom_namespace: nil, managed_namespace: false)
+    with(source_project:, target_cluster:, custom_name:, custom_namespace:, managed_namespace:).reduce(
       CanineConfig::CreateDefinition,
       CanineConfig::RestoreProject,
       CanineConfig::Initialize
