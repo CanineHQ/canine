a302fe82cbde80df38b3564957b40864b3c83931
Chris <chris@example.com>
2026-06-02 10:34:39 +0100

    Hide Portainer onboarding in cluster mode

--- spec/requests/root_routing_spec.rb
+++ spec/requests/root_routing_spec.rb
@@ -25,5 +25,7 @@ RSpec.describe "Root routing", type: :request do
 
     expect(response).to have_http_status(:ok)
     expect(response.body).to include("Select your installation method")
+    expect(response.body).to include("Normal (Recommended)")
+    expect(response.body).not_to include("Import from an existing Portainer instance")
   end
 end
