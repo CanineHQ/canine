3aef5468d8da272b2927f673374d4ae78a56b765
Chris <chris@example.com>
2026-08-08 11:10:23 -0700

    fix: always bracket-paste uploaded file path into terminal

--- app/javascript/controllers/terminal_controller.js
+++ app/javascript/controllers/terminal_controller.js
@@ -5,7 +5,7 @@ import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
   static targets = ["container", "status", "dropOverlay", "statusMessage"]
-  static values = { token: String, uploadUrl: String, bracketPaste: Boolean }
+  static values = { token: String, uploadUrl: String }
 
   connect() {
     this.setupTerminal()
@@ -230,12 +230,10 @@ export default class extends Controller {
         if (response.ok) {
           this.showStatusMessage(`Uploaded to ${result.path}`, "success")
           setTimeout(() => this.hideStatusMessage(), 3000)
-          if (this.bracketPasteValue) {
-            this.channel?.send({
-              type: "input",
-              data: `\x1b[200~${result.path}\x1b[201~`,
-            })
-          }
+          this.channel?.send({
+            type: "input",
+            data: `\x1b[200~${result.path}\x1b[201~`,
+          })
         } else {
           this.showStatusMessage(result.error, "error")
           setTimeout(() => this.hideStatusMessage(), 5000)
