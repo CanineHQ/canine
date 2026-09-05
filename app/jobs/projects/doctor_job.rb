7a80cf569fe33e0e4cd62abe8a38f4ac9be39772
Chris <chris@example.com>
2026-09-05 13:31:47 -0400

    Limit doctor job to one at a time per project

--- app/jobs/projects/doctor_job.rb
+++ app/jobs/projects/doctor_job.rb
@@ -1,6 +1,14 @@
 class Projects::DoctorJob < ApplicationJob
   queue_as :default
 
+  good_job_control_concurrency_with(
+    perform_limit: 1,
+    enqueue_limit: 1,
+    key: -> { "doctor_#{arguments.first.id}" }
+  )
+
+  discard_on GoodJob::ActiveJobExtensions::Concurrency::ConcurrencyExceededError
+
   def perform(project, user)
     Projects::Doctor.execute(project: project, user: user)
   end
