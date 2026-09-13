433e58111fac26142fb6a46f56185ce02030eb61
Chris <chris_zhu12@hotmail.com>
2026-09-13 09:11:02 -0400

    Require Configuration::SMTP before autoload is available

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require_relative "../lib/configuration/smtp"
 BOOT_MODES = %w[local cloud cluster]
 
 module Canine
