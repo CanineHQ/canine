aa38fe62a81ba1627c73245747afbd7a5d51ca41
Chris <chris_zhu12@hotmail.com>
2026-09-15 19:04:01 -0400

    Refactor AddOns::Update into organizer with deploy auth proxy step
    Split into: Save, ManageOAuthApplication, DeployAuthProxy actions.
    Add ManageOAuthApplication to AddOns::Create for internal on creation.
    Move auth proxy K8s deployment from controller into action class.

---
+++ app/actions/add_ons/manage_oauth_application.rb
@@ -0,0 +1,21 @@
+class AddOns::ManageOAuthApplication
+  extend LightService::Action
+  expects :add_on, :was_internal
+
+  executed do |context|
+    add_on = context.add_on
+
+    if add_on.internal? && !context.was_internal
+      unless add_on.oauth_application.present?
+        add_on.create_oauth_application!(
+          name: "Auth Proxy: #{add_on.name}",
+          redirect_uri: "#{ENV.fetch('APP_HOST')}/oauth2/callback",
+          scopes: "openid profile",
+          confidential: true
+        )
+      end
+    elsif context.was_internal && !add_on.internal?
+      add_on.oauth_application&.destroy
+    end
+  end
+end
--- app/actions/add_ons/update.rb
+++ app/actions/add_ons/update.rb
@@ -1,26 +1,13 @@
 class AddOns::Update
-  extend LightService::Action
-  expects :add_on
-  promises :add_on
+  extend LightService::Organizer
 
-  executed do |context|
-    add_on = context.add_on
+  def self.call(add_on, connection)
     was_internal = add_on.internal?
-    add_on.save
 
-    # Manage OAuth application for internal auth proxy
-    if add_on.internal? && !was_internal
-      unless add_on.oauth_application.present?
-        add_on.create_oauth_application!(
-          name: "Auth Proxy: #{add_on.name}",
-          redirect_uri: "#{ENV.fetch('APP_HOST')}/oauth2/callback",
-          scopes: "openid profile",
-          confidential: true
-        )
-      end
-    elsif was_internal && !add_on.internal?
-      add_on.oauth_application&.destroy
-      AddOns::CleanupAuthProxyJob.perform_later(add_on) if add_on.installed?
-    end
+    with(add_on:, was_internal:, connection:).reduce(
+      AddOns::Save,
+      AddOns::ManageOAuthApplication,
+      AddOns::DeployAuthProxy
+    )
   end
 end
