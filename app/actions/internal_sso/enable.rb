45becbe6f5159de4b87b425f8efef8c92737b45c
Chris <chris_zhu12@hotmail.com>
2026-09-18 20:35:52 -0400

    Simplify redirect_uri to always prepend https to APP_HOST

--- app/actions/internal_sso/enable.rb
+++ app/actions/internal_sso/enable.rb
@@ -8,12 +8,9 @@ class InternalSSO::Enable
 
     next context if protectable.oauth_application.present?
 
-    app_host = ENV.fetch("APP_HOST")
-    app_host = "https://#{app_host}" unless app_host.start_with?("http")
-
     protectable.create_oauth_application!(
       name: "Auth Proxy: #{protectable.name}",
-      redirect_uri: "#{app_host}/oauth2/callback",
+      redirect_uri: "https://#{ENV.fetch('APP_HOST')}/oauth2/callback",
       scopes: "openid profile email",
       confidential: true
     )
